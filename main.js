c749cc013242fa4021ee165901a7332af9ef21da
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-07-31 20:50:00 -0400

    Initial commit
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	new file:   .gitignore
    	new file:   ast.json
    	new file:   example.html
    	new file:   main.js
    	new file:   package.json

---
+++ main.js
@@ -0,0 +1,532 @@
+/* globals Deno */
+
+class LineSyntaxError extends Error {
+
+    constructor(message, spec) {
+        super(message);
+
+        Object.defineProperties(this, {
+            name: {
+                enumerable: true,
+                value: 'LineSyntaxError',
+            },
+            message: {
+                enumerable: true,
+                value: message,
+            },
+            line: {
+                enumerable: true,
+                value: spec.line,
+            },
+            lineNumber: {
+                enumerable: true,
+                value: spec.lineNumber,
+            },
+            startPosition: {
+                enumerable: true,
+                value: spec.startPosition,
+            },
+            endPosition: {
+                enumerable: true,
+                value: spec.endPosition,
+            },
+        });
+    }
+
+    toString() {
+        let linePointer = '';
+        const linePointerLength = this.lineNumber.toString().length + 2 + this.startPosition;
+
+        for (let i = 0; i < linePointerLength; i += 1) {
+            linePointer += ' ';
+        }
+
+        linePointer += '^';
+
+        const lines = [
+            `${ this.name }: ${ this.message }`,
+            `${ this.lineNumber }: ${ this.line.trimEnd() }`,
+            linePointer,
+            '',
+        ];
+
+        return lines.join('\n');
+    }
+}
+
+function tokenize(utf8) {
+    const lines = utf8.split('\n');
+    const errors = [];
+    const tokens = [];
+
+    let lineIndex = 0;
+
+    for (; lineIndex < lines.length; lineIndex += 1) {
+        // Add the newline character back after splitting it out.
+        const line = lines[lineIndex] + '\n';
+
+        let startPosition = 0;
+        let tokenString = '';
+
+        let position = 0;
+
+        for (; position < line.length; position += 1) {
+            const char = line[position];
+
+            if (char === '{') {
+                if (tokenString === '{{') {
+                    // This is a non-escaped HTML string value.
+                    tokens.push({
+                        line,
+                        lineNumber: lineIndex + 1,
+                        startPosition,
+                        endPosition: position,
+                        tokenString: '{{{',
+                    });
+
+                    tokenString = '';
+                    startPosition = position + 1;
+                } else if (tokenString.endsWith('{')) {
+                    // Pop off the "{" char, which indicated we are ready to enter a mustache, and
+                    // throw it away.
+                    tokenString = tokenString.slice(0, -1);
+
+                    tokens.push({
+                        line,
+                        lineNumber: lineIndex + 1,
+                        startPosition,
+                        // The end position of the previous token is now 2 places backward.
+                        endPosition: position - 2,
+                        tokenString,
+                    });
+
+                    // The new token string is a mustache.
+                    tokenString = '{{';
+                    startPosition = position - 1;
+                } else {
+                    tokenString += char;
+                }
+            } else if (char === '}') {
+                if (tokenString === '}}') {
+                    // This is a non-escaped HTML string value.
+                    tokens.push({
+                        line,
+                        lineNumber: lineIndex + 1,
+                        startPosition,
+                        endPosition: position,
+                        tokenString: '}}}',
+                    });
+
+                    startPosition = position + 1;
+                    tokenString = '';
+                } else if (tokenString.endsWith('}')) {
+                    // Pop off the "}" char, which indicated we are ready to enter a mustache, and
+                    // throw it away.
+                    tokenString = tokenString.slice(0, -1);
+
+                    tokens.push({
+                        line,
+                        lineNumber: lineIndex + 1,
+                        startPosition,
+                        // The end position of the previous token is now 2 places backward.
+                        endPosition: position - 2,
+                        tokenString,
+                    });
+
+                    // The new token string is a mustache.
+                    tokenString = '}}';
+                    startPosition = position - 1;
+                } else {
+                    tokenString += char;
+                }
+            } else if (tokenString === '{{' || tokenString === '}}') {
+                tokens.push({
+                    line,
+                    lineNumber: lineIndex + 1,
+                    startPosition,
+                    endPosition: position - 1,
+                    tokenString,
+                });
+
+                startPosition = position;
+                tokenString = char;
+            } else {
+                tokenString += char;
+            }
+        }
+
+        tokens.push({
+            line,
+            lineNumber: lineIndex + 1,
+            startPosition,
+            endPosition: position - 1,
+            tokenString,
+        });
+    }
+
+    return { tokens, errors };
+}
+
+function buildSyntaxTree(tokens) {
+    const errors = [];
+    const tree = [];
+    const blocks = [];
+
+    let previousToken = null;
+    let currentLine = 0;
+    let mustacheOpenToken = null;
+    let expectingExpression = false;
+    let expectingMustacheClose = false;
+
+    function resetLineNumber(newLineNumber) {
+        if (currentLine !== newLineNumber) {
+            currentLine = newLineNumber;
+            return true;
+        }
+
+        return false;
+    }
+
+    function pushNode(node) {
+        if (node.type === 'EXPRESSION' && node.exp.length > 1) {
+            // If a node has more than one sub expression, we can assume it
+            // is a helper expression.
+            node.type = 'HELPER_EXPRESSION';
+        }
+        if (node.type === 'EXPRESSION' && node.exp.length === 1) {
+            const exp = node.exp[0];
+            // eslint-disable-next-line no-useless-escape
+            if (exp.type === 'PATH' && /[\.\[\]]+/.test(exp.path)) {
+                // If a path has dereferencing characters in it ([,.,]) then we can assume it
+                // is a path expression.
+                node.type = 'PATH_EXPRESSION';
+            }
+        }
+
+        if (blocks.length > 0) {
+            blocks[blocks.length - 1].children.push(node);
+        } else {
+            tree.push(node);
+        }
+    }
+
+    function pushBlockOpen(node) {
+        if (blocks.length > 0) {
+            blocks[blocks.length - 1].children.push(node);
+        } else {
+            tree.push(node);
+        }
+
+        blocks.push(node);
+    }
+
+    function pushBlockClose() {
+        blocks.pop();
+    }
+
+    function addExpression(token) {
+        const tokenString = token.tokenString.trim();
+        const encodeMarkup = mustacheOpenToken.tokenString === '{{';
+
+        switch (tokenString[0]) {
+            case '#': // Open Block Helper
+                pushBlockOpen({
+                    type: 'BLOCK_OPEN',
+                    exp: parseExpression(token, tokenString.slice(1).trim()),
+                    children: [],
+                    token,
+                });
+                break;
+            case '/': // Close Block Helper
+                pushBlockClose({
+                    type: 'BLOCK_CLOSE',
+                    exp: tokenString.slice(1).trim(),
+                    token,
+                });
+                break;
+            case '>': // Partial
+                pushNode({
+                    type: 'PARTIAL',
+                    exp: tokenString.slice(1).trim(),
+                    token,
+                });
+                break;
+            default:
+                if (tokenString === 'else') {
+                    pushNode({ type: 'ELSE', token });
+                } else {
+                    pushNode({
+                        type: 'EXPRESSION',
+                        exp: parseExpression(token, tokenString),
+                        encodeMarkup,
+                        token,
+                    });
+                }
+        }
+    }
+
+    function addContent(token) {
+        pushNode({
+            type: 'CONTENT',
+            str: token.tokenString,
+            token,
+        });
+    }
+
+    function parseExpression(token, str) {
+        const expTokens = [];
+        let subToken = null;
+        let expTokenString = '';
+        let inStringLiteral = false;
+        let inBlockParams = false;
+
+        function parseJavaScriptType() {
+            const rv = { type: 'LITERAL', value: expTokenString };
+            let n;
+
+            if (inStringLiteral) {
+                return rv;
+            }
+
+            switch (expTokenString) {
+                case 'true':
+                    rv.value = true;
+                    return rv;
+                case 'false':
+                    rv.value = false;
+                    return rv;
+                case 'null':
+                    rv.value = null;
+                    return rv;
+                case 'undefined':
+                    // eslint-disable-next-line no-undefined
+                    rv.value = undefined;
+                    return rv;
+                default:
+                    n = Number.parseInt(expTokenString, 10);
+                    if (Number.isNaN(n)) {
+                        n = Number.parseFloat(expTokenString);
+                    }
+
+                    if (Number.isNaN(n)) {
+                        rv.type = 'PATH';
+                        rv.path = expTokenString;
+                    } else {
+                        rv.value = n;
+                    }
+
+                    return rv;
+            }
+        }
+
+        function closeSubToken() {
+            const value = parseJavaScriptType();
+
+            if (subToken && subToken.type === 'KEY_VALUE') {
+                subToken.value = value;
+            } else if (value.type === 'PATH') {
+                subToken = {
+                    type: 'PATH',
+                    path: value.path,
+                };
+            } else {
+                subToken = {
+                    type: 'LITERAL',
+                    value: value.value,
+                };
+            }
+
+            expTokens.push(subToken);
+            expTokenString = '';
+            subToken = null;
+        }
+
+        for (const c of str) {
+            if (inBlockParams) {
+                if (c === '|') {
+                    expTokens.push({
+                        type: 'BLOCK_PARAMS',
+                        params: parseExpression(token, expTokenString.trim()),
+                    });
+
+                    expTokenString = '';
+                    inBlockParams = false;
+                } else {
+                    expTokenString += c;
+                }
+            } else if (inStringLiteral) {
+                if (c === '"') {
+                    closeSubToken();
+                    inStringLiteral = false;
+                } else {
+                    expTokenString += c;
+                }
+            } else if (c === '"') {
+                inStringLiteral = true;
+            } else if (c === '=') {
+                subToken = {
+                    type: 'KEY_VALUE',
+                    key: expTokenString,
+                };
+
+                expTokenString = '';
+            } else if (/\s/.test(c)) {
+                if (expTokenString) {
+                    closeSubToken();
+                }
+            } else if (c === '|') {
+                subToken = expTokens[expTokens.length - 1];
+
+                if (subToken && subToken.type === 'PATH' && subToken.path === 'as') {
+                    inBlockParams = true;
+                    expTokens.pop();
+                    subToken = null;
+                    expTokenString = '';
+                }
+            } else {
+                expTokenString += c;
+            }
+        }
+
+        if (inBlockParams) {
+            errors.push(new LineSyntaxError(`Unclosed block params on line ${ previousToken.lineNumber }`, {
+                line: token.line,
+                lineNumber: token.lineNumber,
+                startPosition: token.startPosition,
+                endPosition: token.endPosition,
+            }));
+        } else if (expTokenString) {
+            closeSubToken();
+        }
+
+        return expTokens;
+    }
+
+    function isClosingMustache(token) {
+        return token.tokenString === '}}' || token.tokenString === '}}}';
+    }
+
+    function isOpeningMustache(token) {
+        return token.tokenString === '{{' || token.tokenString === '{{{';
+    }
+
+    for (const token of tokens) {
+        if (resetLineNumber(token.line) && mustacheOpenToken) {
+            errors.push(new LineSyntaxError(`Unclosed mustache on line ${ previousToken.lineNumber }`, {
+                line: previousToken.line,
+                lineNumber: previousToken.lineNumber,
+                startPosition: mustacheOpenToken.startPosition,
+                endPosition: mustacheOpenToken.endPosition,
+            }));
+
+            mustacheOpenToken = null;
+            expectingMustacheClose = false;
+            expectingExpression = false;
+        }
+
+        if (expectingMustacheClose && !isClosingMustache(token)) {
+            errors.push(new LineSyntaxError(`Expected closing mustache but got ${ token.tokenString } on line ${ token.lineNumber }`, {
+                line: token.line,
+                lineNumber: token.lineNumber,
+                startPosition: token.startPosition,
+                endPosition: token.endPosition,
+            }));
+            expectingMustacheClose = false;
+        } else if (isClosingMustache(token)) {
+            mustacheOpenToken = null;
+            expectingExpression = false;
+            expectingMustacheClose = false;
+        } else if (isOpeningMustache(token)) {
+            if (expectingExpression) {
+                errors.push(new LineSyntaxError(`Expected expression but got ${ token.tokenString } on line ${ token.lineNumber }`, {
+                    line: token.line,
+                    lineNumber: token.lineNumber,
+                    startPosition: token.startPosition,
+                    endPosition: token.endPosition,
+                }));
+            } else {
+                mustacheOpenToken = token;
+                expectingExpression = true;
+            }
+        } else if (expectingExpression) {
+            addExpression(token);
+            expectingExpression = false;
+            expectingMustacheClose = true;
+        } else {
+            addContent(token);
+        }
+
+        previousToken = token;
+    }
+
+    return { tree, errors };
+}
+
+// Used for scope testing below
+const baz = 3; // eslint-disable-line no-unused-vars
+
+function createFunction() {
+
+    /* eslint-disable */
+    const body = [
+        'const parts = [];',
+        'function push(s) { parts.push(s); }',
+        'with (data) {',
+        // Will be "object" unless there is a `console` member on the data object.
+        'push(`typeof console: ${typeof console}`);',
+        // Will be undefined
+        'push(`typeof this.console: ${typeof this.console}`);',
+        // Will be number
+        'push(`typeof foo: ${typeof foo}`);',
+        // Will be undefined; not on the data object.
+        'push(`typeof bar: ${typeof bar}`);',
+        // Will be undefined; cannot access module values.
+        'push(`typeof baz: ${typeof baz}`);',
+        // Will be string; getting this off the data object.
+        'push(`typeof foobar: ${typeof foobar}`);',
+        // Will be undefined; not available on the context.
+        'push(`typeof this.foo: ${typeof this.foo}`);',
+        // Will be number; is defined on the context
+        'push(`typeof this.bar: ${typeof this.bar}`);',
+        // Will be undefined; cannot access module values.
+        'push(`typeof this.baz: ${typeof this.baz}`);',
+        // Will be number; defined on the context.
+        'push(`typeof this.foobar: ${typeof this.foobar}`);',
+        '}',
+        'return parts.join("\\n");',
+    ].join('\n');
+
+    return new Function('data', body);
+    /* eslint-enable */
+}
+
+// eslint-disable-next-line no-unused-vars
+function main_testFunctionScope() {
+    const data = { foo: 1, console: 'foo', foobar: 'three' };
+    const context = { bar: 2, foobar: 3 };
+
+    const fn = createFunction().bind(context);
+
+    console.log(fn(data));
+}
+
+async function main() {
+    const utf8 = await Deno.readTextFile('./example.html');
+    const { tokens } = tokenize(utf8);
+
+    // Uncomment to view raw tokens
+    // tokens.forEach((item) => {
+    //     console.log(item);
+    // });
+
+    // Uncomment to save the AST as JSON.
+    const { tree, errors } = buildSyntaxTree(tokens);
+
+    errors.forEach((err) => {
+        console.log(err.toString());
+    });
+
+    await Deno.writeTextFile('./ast.json', JSON.stringify(tree, null, 2));
+}
+
+main();
