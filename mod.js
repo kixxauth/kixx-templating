e969fd436e67f1523b34ecb4a6f985745f6f178a
Kris Walker <kris@kixx.name>
2025-07-17 18:14:54 -0400

    Export escapeHTMLChars utility
     Changes to be committed:
    	modified:   mod.js

--- mod.js
+++ mod.js
@@ -2,10 +2,12 @@ import tokenize from './lib/tokenize.js';
 import buildSyntaxTree from './lib/build-syntax-tree.js';
 import createRenderFunction from './lib/create-render-function.js';
 import helpers from './lib/helpers/mod.js';
+import { escapeHTMLChars } from './lib/utils.js';
 
 export {
     tokenize,
     buildSyntaxTree,
     createRenderFunction,
-    helpers
+    helpers,
+    escapeHTMLChars
 };
